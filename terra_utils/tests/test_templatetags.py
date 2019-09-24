4f2be8d218119fbb667707038b4d8d51161525fa
J-E Castagnede <j.e.castagnede@gmail.com>
2019-09-24 18:34:06 +0200

    fix travis

--- terra_utils/tests/test_templatetags.py
+++ terra_utils/tests/test_templatetags.py
@@ -1,12 +1,10 @@
 from django.template import Context, Template
 from django.test import TestCase
-from django.test.utils import override_settings
+
+from terra_utils import settings
 
 
 class TemplatesTagsTestCase(TestCase):
-    front_url = 'http://myfront/'
-
-    @override_settings(FRONT_URL=front_url)
     def test_front_url(self):
         context = Context()
         template_to_render = Template(
@@ -16,4 +14,4 @@ class TemplatesTagsTestCase(TestCase):
 
         rendered = template_to_render.render(context)
 
-        self.assertEqual(rendered, self.front_url)
+        self.assertEqual(rendered, settings.FRONT_URL)
